05284d61b5afb7bf9acb8d4862b019de7aea101f
vladsaveliev <vladislav.sav@gmail.com>
2018-08-23 00:31:17 +1000

    Add setup.py and VERSION

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import sys
+py_v = sys.version_info[:2]
+if py_v < (3, 6):
+    sys.exit('Only Python 3.6 and higher are supported. Current version: ' + '.'.join(py_v))
+
+from os.path import join
+
+name = 'test_travis'
+script_name = 'test_travis'
+package_name = 'test_travis'
+
+with open('VERSION.txt') as f:
+    version = f.read().strip().split('\n')[0]
+
+from setuptools import setup, find_packages
+setup(
+    name=name,
+    version=version,
+    author='Vlad Saveliev and Alla Mikheenko',
+    author_email='vladislav.sav@gmail.com',
+    description='Genome capture target coverage evaluation tool',
+    long_description=(open('README.md').read()),
+    keywords='bioinformatics',
+    url='https://github.com/vladsaveliev/bed_annotation',
+    license='GPLv3',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=[],
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: JavaScript',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
