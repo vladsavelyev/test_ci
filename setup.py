44b7353762cefc2fcea66739e805e2b564730f8b
vladsaveliev <vladislav.sav@gmail.com>
2020-07-24 00:15:16 +1000

    Update CI

--- setup.py
+++ setup.py
@@ -1,30 +1,46 @@
 #!/usr/bin/env python
-from os.path import join
-from setuptools import setup, find_packages
-from versionpy import get_version, find_package_files, get_reqs
+import os
+from setuptools import setup
 
 pkg = 'test_ci'
-version = get_version(pkg)
+
+try:
+    import versionpy
+except ImportError:
+    res = input('Installation requires versionpy. Install it now? [Y/n]')
+    if res.lower().startswith('n'):
+        raise
+    os.system('pip install versionpy')
+    import versionpy
+
+version = versionpy.get_version(pkg)
+package_data = {
+    pkg: versionpy.find_package_files('', pkg)
+}
 
 setup(
     name=pkg,
     script_name=pkg,
     version=version,
-    author='Vlad Saveliev',
+    author='Vlad Savelyev',
     author_email='vladislav.sav@gmail.com',
-    description='Genome capture target coverage evaluation tool',
+    description='Toy repo to test CI',
     long_description=(open('README.md').read()),
     long_description_content_type="text/markdown",
-    url='https://github.com/vladsaveliev/test_ci',
+    url=f'https://github.com/vladsaveliev/{pkg}',
     license='GPLv3',
-    packages=find_packages(),
+    packages=[pkg],
+    package_data=package_data,
     include_package_data=True,
     zip_safe=False,
-    setup_requires=[],
+    install_requires=[
+        'versionpy',
+        'click',
+        'ngs_utils'
+    ],
     keywords='bioinformatics',
     classifiers=[
         'Environment :: Console',
-        'Environment :: Web Environment',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
