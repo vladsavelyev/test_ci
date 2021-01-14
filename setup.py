c089a708fc681d27f20b73c06abd2d9f2ed78def
vladsaveliev <vladislav.sav@gmail.com>
2021-01-14 18:36:45 +1100

    Bump version: 2.0.6 → 2.0.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ pkg = 'test_ci'
 setup(
     name=pkg,
     script_name=pkg,
-    version='2.0.6',
+    version='2.0.7',
     author='Vlad Savelyev',
     author_email='vladislav.sav@gmail.com',
     description='Toy repo to test CI',
